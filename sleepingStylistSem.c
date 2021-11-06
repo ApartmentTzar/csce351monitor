85f956f68f64c31b8aedf8fb0c279de2860cb49a
tmcelmury <trentamcelmury@gmail.com>
2021-11-05 22:01:11 -0500

    almost there

--- sleepingStylistSem.c
+++ sleepingStylistSem.c
@@ -5,8 +5,8 @@
 #include <semaphore.h>
 /* // ==== s l e e p i n g S t y l i s t S e m . c ====// */
 # define CHAIRS 6 
-# define DELAY 1000000 // adjust this value
-# define NUM 76
+# define DELAY 500000 // adjust this value
+# define NUM 16
 
 sem_t mutex;
 sem_t stylist;
@@ -22,45 +22,50 @@ void style ( void )
     sem_wait(&customers);
     sem_wait(&mutex);
     waiting--;
-    printf("A new customer is being seen.")
+    printf("A new customer is being seen.\n%i customers waiting.\n", waiting);
     sem_post(&stylist);
     sem_post(&mutex) ;
     for ( j = 0 ; j < DELAY; j++ ); //cut hair
   }
 }
-void customer( void )
+void customer( void * id )
 {
   int j ;
+  int* threadId = (int *)id;
   while ( 1 ) {
-    sem_wait(&mutex) ;
+    sem_wait(&mutex);
+    printf("Customer %i checked in.\n", *threadId);
     if ( waiting < CHAIRS ) {
       waiting++;
-      printf("%i customers waiting.\n", waiting);
+      printf("%i customer(s) waiting.\n", waiting);
       sem_post(&customers);
       sem_post(&mutex);
       sem_wait(&stylist);
       break ;
     }
     else {
+      printf("Customer %i went shopping.\n", *threadId);
       sem_post(&mutex);
       for ( j = 0 ; j < DELAY; j++ ); //go shopping
     }	
   }
-  printf("Thread %i got a haircut.\n", pthread_self());
+  printf("Customer %i got a haircut.\n", *threadId);
 }
 
 void main( void )
 {
   sem_init(&mutex, 0, 1);
-  sem_init(&stylist, 0, 1);
-  sem_init(&customers, 0, 1);
+  sem_init(&stylist, 0, 0);
+  sem_init(&customers, 0, 0);
   pthread_t thread[NUM];
   for (int i = 0; i < NUM; i++) {
     if (i == 0) {
       pthread_create(&thread[i], NULL, (void *) style, NULL);
     }
     else {
-      pthread_create(&thread[i], NULL, (void *) customer, NULL);
+      int * id = (int *)malloc(sizeof(int));
+			*id = i;
+			pthread_create(&thread[i], NULL, (void *) customer, (void *)id);
     }
   }
   for (int i = 0; i < NUM; i++) {
