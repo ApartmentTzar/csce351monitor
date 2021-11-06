7de8e0d22c6cb46ef75fe4948976e061a4c4ae7a
Kevin Kelvin Wertz <kwertz@csce.cs.unl.edu>
2021-11-06 02:43:09 -0500

    custom cond variables seem to work, is probably SC monitor

--- sleepingStylistMon.c
+++ sleepingStylistMon.c
@@ -10,50 +10,46 @@
 #define NUM 20
 
 
-pthread_cond_t barber;
-pthread_cond_t customers;
+//pthread_cond_t barber;
+//pthread_cond_t customers;
 pthread_mutex_t lock;
 int waiting = 0;
+sem_t entryQueue;
+//int stylists = 0;
 bool sleeping = false;
 
 typedef struct cv 
 {
 	int waiting;
 	sem_t semaphore;
-	pthread_mutex_t lock;
 }cv;
 
-cv cond;
+cv barber;
+cv customers;
 
-struct cv initCond()
+void initCond(cv * cond,int count)
 {
-	cv newCond;
-	sem_init(&newCond.semaphore,0,CHAIRS);
-	pthread_mutex_init(&newCond.lock,NULL);
-	return newCond;
+	sem_init(&cond->semaphore,0,count);
 }
 
-int count(cv c)
+int count(cv* c)
 {
-	return c.waiting;
+	return c->waiting;
 }
 
-void wait(cv c)
+void wait(cv* c)
 {
-	pthread_mutex_lock(&c.lock);
-	c.waiting++;
-	pthread_mutex_unlock(&c.lock);
-	sem_wait(&c.semaphore);
+	sem_wait(&c->semaphore);
 	return;
 }
 
-void signal(cv c)
+void signal(cv* c)
 {
-	if(c.waiting>0)
+	if(c->waiting>0)
 	{
-		c.waiting--;
+		c->waiting--;
 	}
-	sem_post(&c.semaphore);
+	sem_post(&c->semaphore);
 	return;
 }
 
@@ -61,38 +57,54 @@ void mon_debugPrint(bool isStylist)
 {
 	if(isStylist)
 	{
-		printf("Stylist Debug: %d customers in chair\n",waiting);
+		//printf("Stylist Debug: %d customers in chair\n",waiting);
 	}
 	else
 	{
-		printf("Customer Debug: %d customers in chair\n",waiting);
+		//printf("Customer Debug: %d customers in chair\n",waiting);
 	}
 }
 
 void mon_checkCustomer()
 {
+	sem_wait(&entryQueue);
 	if(waiting==0)
 	{
 		printf("No customers going to sleep\n");
-		pthread_cond_wait(&barber,&lock);
+		//pthread_cond_wait(&barber,&lock);
+		wait(&barber);
 		printf("Woke up, cutting hair\n");
 	}
-	--waiting;
-	pthread_cond_signal(&customers);
+	else
+	{
+		waiting--;
+		printf("%d customers in shop\n",waiting);
+		//pthread_cond_signal(&customers);
+		signal(&customers);
+		//sem_post(&entryQueue);
+	}
+	sem_post(&entryQueue);
+	return;
 }
 
 bool mon_checkStylist(int id)
 {
+	sem_wait(&entryQueue);
 	if(waiting<CHAIRS)
 	{
 		waiting++;
+		//pthread_cond_signal(&barber);
+		//signal(&barber);
+		//pthread_cond_wait(&customers,&lock);
+		wait(&customers);
 		printf("Customer %d entering barber shop\n",id);
-		pthread_cond_signal(&barber);
-		pthread_cond_wait(&customers,&lock);
+		printf("%d customers in shop\n",waiting);
+		sem_post(&entryQueue);
 		return true;
 	}
 	else
 	{
+		sem_post(&entryQueue);
 		return false;
 	}
 }
@@ -103,8 +115,8 @@ void stylist(void)
     {
 		mon_debugPrint(true);
 		mon_checkCustomer() ;
-		printf("Cutting hair\n");
 		for(int k = 0; k < DELAY; k++); // cut hair
+		printf("Gave Haircut\n");
     }
 }
 
@@ -123,9 +135,12 @@ void customer(void *id)
 
 void main (void)
 {
-	pthread_cond_init(&barber,NULL);
-	pthread_cond_init(&customers,NULL);
+	//pthread_cond_init(&barber,NULL);
+	//pthread_cond_init(&customers,NULL);
 	pthread_mutex_init(&lock,NULL);
+	sem_init(&entryQueue,0,1);
+	initCond(&barber,1);
+	initCond(&customers,CHAIRS);
 	//cond = initCond();
 	pthread_t thread[NUM];
 	for (int i = 0; i < NUM; i++) 
