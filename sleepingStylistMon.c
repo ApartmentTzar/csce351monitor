2be067eb30422ef3f60e63b9d8e1148e2a98c474
Kevin Kelvin Wertz <kwertz@csce.cs.unl.edu>
2021-11-06 20:02:22 -0500

    incorperated some feedback from prof and fixed some bugs

--- sleepingStylistMon.c
+++ sleepingStylistMon.c
@@ -10,13 +10,13 @@
 #define NUM 20
 
 
-//pthread_cond_t barber;
-//pthread_cond_t customers;
-pthread_mutex_t lock;
-int waiting = 0;
 sem_t entryQueue;
-//int stylists = 0;
-bool sleeping = false;
+int stylists = 0;
+int customers = 0;
+
+int haircutsGiven = 0;
+int timesFull = 0;
+int timesEmpty = 0;
 
 typedef struct cv 
 {
@@ -24,8 +24,8 @@ typedef struct cv
 	sem_t semaphore;
 }cv;
 
-cv barber;
-cv customers;
+cv stylistAvailable;
+cv customerAvailable;
 
 void initCond(cv * cond,int count)
 {
@@ -39,84 +39,83 @@ int count(cv* c)
 
 void wait(cv* c)
 {
+    sem_post(&entryQueue);
+	c->waiting++;
 	sem_wait(&c->semaphore);
-	return;
+	c->waiting--;
+    sem_wait(&entryQueue);
 }
 
 void signal(cv* c)
 {
-	if(c->waiting>0)
-	{
-		c->waiting--;
-	}
 	sem_post(&c->semaphore);
-	return;
 }
 
-void mon_debugPrint(bool isStylist)
+void mon_debugPrint()
 {
-	if(isStylist)
-	{
-		//printf("Stylist Debug: %d customers in chair\n",waiting);
-	}
-	else
-	{
-		//printf("Customer Debug: %d customers in chair\n",waiting);
-	}
+	sem_wait(&entryQueue);
+	printf("\n");
+    printf("Customers in Salon = %d\n",count(&stylistAvailable));
+    printf("Given haircuts     = %d\n",haircutsGiven);
+    printf("Salon full         = %d\n",timesFull);
+    printf("Salon empty        = %d\n",timesEmpty);
+	printf("\n");
+	sem_post(&entryQueue);
 }
 
 void mon_checkCustomer()
 {
-	sem_wait(&entryQueue);
-	if(waiting==0)
-	{
-		printf("No customers going to sleep\n");
-		//pthread_cond_wait(&barber,&lock);
-		wait(&barber);
-		printf("Woke up, cutting hair\n");
-	}
-	else
-	{
-		waiting--;
-		printf("%d customers in shop\n",waiting);
-		//pthread_cond_signal(&customers);
-		signal(&customers);
-		//sem_post(&entryQueue);
-	}
-	sem_post(&entryQueue);
-	return;
+    sem_wait(&entryQueue);
+	stylists = stylists + 1;
+    signal(&stylistAvailable);
+    if(count(&stylistAvailable) <= 0)
+    {
+        printf("No customers going to sleep\n");
+        timesEmpty++;
+        wait(&customerAvailable);
+    	printf("Woke up, cutting hair\n");
+
+    }
+    customers=customers - 1;
+    sem_post(&entryQueue);
 }
 
 bool mon_checkStylist(int id)
 {
-	sem_wait(&entryQueue);
-	if(waiting<CHAIRS)
-	{
-		waiting++;
-		//pthread_cond_signal(&barber);
-		//signal(&barber);
-		//pthread_cond_wait(&customers,&lock);
-		wait(&customers);
-		printf("Customer %d entering barber shop\n",id);
-		printf("%d customers in shop\n",waiting);
-		sem_post(&entryQueue);
-		return true;
-	}
-	else
-	{
-		sem_post(&entryQueue);
-		return false;
-	}
+    sem_wait(&entryQueue);
+	bool status = false;
+    if(count(&stylistAvailable) < CHAIRS)
+    {
+        customers = customers + 1;
+        if(count(&stylistAvailable)==CHAIRS-1)
+        {
+            timesFull++;
+        }
+        printf("Customer %d entering barber shop\n",id);
+		if(count(&customerAvailable)>0)
+		{
+			signal(&customerAvailable);
+		}
+        wait(&stylistAvailable);
+		if(stylists > 0)
+		{
+        	stylists = stylists - 1;
+        	status = true;
+		}
+    }
+    sem_post(&entryQueue);
+    return status;
 }
 
 void stylist(void)
 {
     while (1) 
     {
-		mon_debugPrint(true);
+		mon_debugPrint();
 		mon_checkCustomer() ;
 		for(int k = 0; k < DELAY; k++); // cut hair
 		printf("Gave Haircut\n");
+        haircutsGiven++;
     }
 }
 
@@ -125,7 +124,7 @@ void customer(void *id)
 	int *i = (int *)id;
 	while (1)
 	{
-		mon_debugPrint(false);
+		mon_debugPrint();
 		if (mon_checkStylist(*i))
 			break;
 		printf("Customer %d gone shopping\n",*i);
@@ -135,13 +134,11 @@ void customer(void *id)
 
 void main (void)
 {
-	//pthread_cond_init(&barber,NULL);
-	//pthread_cond_init(&customers,NULL);
-	pthread_mutex_init(&lock,NULL);
+
 	sem_init(&entryQueue,0,1);
-	initCond(&barber,1);
-	initCond(&customers,CHAIRS);
-	//cond = initCond();
+	initCond(&stylistAvailable,1);
+	initCond(&customerAvailable,0);
+
 	pthread_t thread[NUM];
 	for (int i = 0; i < NUM; i++) 
 	{
